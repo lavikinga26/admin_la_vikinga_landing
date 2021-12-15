e349cba77ec7581a0467283cf4b97a4ad325ab88
Paolo <paofab_98@outlook.com>
2021-12-15 18:23:47 -0500

    ...

--- src/pages/User/components/ProfileCard.vue
+++ src/pages/User/components/ProfileCard.vue
@@ -1,26 +1,24 @@
 <template>
     <div class="my-5">
         <v-card class="my-10 rounded-xl pa-10">
-            <v-row>
-                <v-col cols="12" md="3" sm="12" align="center">
-                    <div>
+            <v-form ref="profileForm" v-model="validProfileForm" @submit.prevent="saveProfileReg()" lazy-validation>
+                <v-row>
+                    <v-col cols="12" md="3" sm="12" align="center">
                         <v-avatar class="profile rounded-circle" color="grey" size="164" tile>
                             <v-img 
-                                v-if="business_partner.file_path" 
-                                :src="base_url + business_partner.file_path.path + business_partner.file_path.filename">
+                                v-if="profileForm.file_path" 
+                                :src="base_url + profileForm.file_path.path + profileForm.file_path.filename">
                             </v-img>
                         </v-avatar>
-                        <h3 class="mx-10 my-5"><b>{{business_partner.name}} {{business_partner.lastname}}</b></h3>
-                    </div>
-                </v-col>
-                <v-col cols="12" md="9" sm="12" class="px-15">
-                    <div>
+                        <h3 class="mx-10 my-5"><b>{{profileForm.name}} {{profileForm.lastname}}</b></h3>
+                    </v-col>
+                    <v-col cols="12" md="9" sm="12" class="px-15">
                         <div class="tit_h1_staff_pink text_entrena txt_uppercase mb-6">MI INFORMACIÓN PERSONAL</div>
                         <v-row class="align-center">
                             <v-col cols="12" md="4" sm="12"><span style="font-size: 0.9em;">NOMBRES</span></v-col>
                             <v-col cols="12" md="8" sm="12">
                                 <v-text-field
-                                    v-model="business_partner.name"
+                                    v-model="profileForm.name"
                                     class="ma-0 pt-0"
                                     hide-details
                                     single-line
@@ -30,7 +28,7 @@
                             <v-col cols="12" md="4" sm="12"><span style="font-size: 0.9em;">APELLIDOS</span></v-col>
                             <v-col cols="12" md="8" sm="12">
                                 <v-text-field
-                                    v-model="business_partner.lastname"
+                                    v-model="profileForm.lastname"
                                     class="ma-0 pt-0"
                                     hide-details
                                     placeholder="Ingresa aquí tu apellido completo"
@@ -39,7 +37,7 @@
                             <v-col cols="12" md="4" sm="12"><span style="font-size: 0.9em;">DNI</span></v-col>
                             <v-col cols="12" md="8" sm="12">
                                 <v-text-field
-                                    v-model="business_partner.nro_doc"
+                                    v-model="profileForm.nro_doc"
                                     class="ma-0 pt-0"
                                     hide-details
                                     placeholder="Ingresa aquí tu DNI"
@@ -48,7 +46,7 @@
                             <v-col cols="12" md="4" sm="12"><span style="font-size: 0.9em;">Teléfono</span></v-col>
                             <v-col cols="12" md="8" sm="12">
                                 <v-text-field
-                                    v-model="business_partner.phone"
+                                    v-model="profileForm.phone"
                                     class="ma-0 pt-0"
                                     hide-details
                                     placeholder="Ingresa aquí tu Teléfono"
@@ -66,66 +64,61 @@
                             <v-col cols="12" md="4" sm="12"><span style="font-size: 0.9em;">GÉNERO</span></v-col>
                             <v-col cols="12" md="8" sm="12">
                                 <v-radio-group
-                                    row
-                                    hide-details
+                                    v-model="infoPersonal.gender"
                                     class="ma-0 pt-0"
-                                >
+                                    row
+                                    hide-details>
                                     <v-radio
                                         label="Femenino"
-                                        value="radio-1"
+                                        :value="1"
                                     ></v-radio>
                                     <v-radio
                                         label="Masculino"
-                                        value="radio-2"
+                                        :value="2"
                                     ></v-radio>
                                     <v-radio
                                         label="Otro"
-                                        value="radio-3"
+                                        :value="3"
                                     ></v-radio>
                                 </v-radio-group>
                             </v-col>
                             <v-col cols="12" md="4" sm="12"><span style="font-size: 0.9em;">ESTADO CIVIL</span></v-col>
                             <v-col cols="12" md="8" sm="12">
                                 <v-radio-group
-                                    row
-                                    hide-details
+                                    v-model="infoPersonal.civil_status"
                                     class="ma-0 pt-0"
-                                >
+                                    row
+                                    hide-details>
                                     <v-radio
                                         label="Soltera(o)"
-                                        value="radio-1"
+                                        :value="1"
                                     ></v-radio>
                                     <v-radio
-                                        label="Casado(0)"
-                                        value="radio-2"
+                                        label="Casada(o)"
+                                        :value="2"
                                     ></v-radio>
                                     <v-radio
-                                        label="Viudo(0)"
-                                        value="radio-3"
+                                        label="Viuda(o)"
+                                        :value="3"
                                     ></v-radio>
                                 </v-radio-group>
                             </v-col>
                             <v-col cols="12" md="4" sm="12"><span style="font-size: 0.9em;">MADRE/PADRE FAMILIA</span></v-col>
-                            <v-col cols="12" md="8" sm="12">
-                                <v-radio-group
-                                    row
+                            <v-col cols="12" md="4" sm="12">
+                                <v-switch
+                                    v-model="infoPersonal.family_guy"
+                                    :label="`${infoPersonal.family_guy != true ? 'No' : 'Si'}`"
                                     hide-details
-                                    class="ma-0 pt-0"
-                                >
-                                    <v-radio
-                                        label="Sí"
-                                        value="radio-1"
-                                    ></v-radio>
-                                    <v-radio
-                                        label="No"
-                                        value="radio-2"
-                                    ></v-radio>
-                                </v-radio-group>
+                                ></v-switch>
                             </v-col>
                         </v-row>
-                    </div>
-                </v-col>
-            </v-row>
+                    </v-col>
+                </v-row>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn type="submit" color="primary" :disabled="!validProfileForm">Guardar</v-btn>
+                </v-card-actions>
+            </v-form>
         </v-card>
     </div>
 </template>
@@ -142,6 +135,10 @@ export default {
         },
     },
     data: () => ({
+        validProfileForm: false,
+        profileForm:{
+            info_personal: null,
+        },
         infoPersonal:{
         },
     }),
@@ -150,10 +147,25 @@ export default {
     },
     methods: {
         configPersonalInfo(){
+            this.profileForm = Object.assign({}, this.business_partner);
             if(this.business_partner.partner_information.info_personal){
                 this.infoPersonal = JSON.parse(this.business_partner.partner_information.info_personal);
             }
-        }
+        },
+        async saveProfileReg(){
+            try {
+                this.$store.commit('loader', true);
+
+                this.profileForm.info_personal  = JSON.stringify(this.infoPersonal);
+
+            } catch (e) {
+                UTILS.toastr.error("Ups! Ocurrió un error", this);
+                console.error(e);
+
+            } finally {
+                this.$store.commit('loader', false);
+            }
+        },
     }
 }
 </script>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,57 +1,80 @@
 const KEY_LENGHT= 3;
 const UTILS = {
     chrypter: {
-      encode(val) {
-        let key = UTILS.str.rand(KEY_LENGHT)
-        return window.base64.encode(val+key)
-      },
-      decode(val) {
-      
-        let str = window.base64.decode(val)
-        return str.substring(0,str.length-KEY_LENGHT);
-      },
-      
+        encode(val) {
+            let key = UTILS.str.rand(KEY_LENGHT)
+            return window.base64.encode(val+key)
+        },
+        decode(val) {
+            let str = window.base64.decode(val)
+            return str.substring(0,str.length-KEY_LENGHT);
+        },
     },
     str:{
-      rand(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        rand(length) {
+            var result           = '';
+            var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var charactersLength = characters.length;
+
+            for ( var i = 0; i < length; i++ ) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
         }
-        return result;
-      }
     },
     filters: {
         only_numbers($event) {
             let keyCode = $event.keyCode ? $event.keyCode : $event.which;
             if ((keyCode < 48 || keyCode > 57) && keyCode !== 46) {
-              $event.preventDefault();
+                $event.preventDefault();
             }
-          },
+        },
+    },
+    toastr:{
+        success(message,vm){
+            vm.$swal({
+                toast: true,
+                position: "top-end",
+                icon: "success",
+                title: message,
+                showConfirmButton: false,
+                timerProgressBar: true,
+                timer: 2500,
+            });
+        },
+        error(message,vm){
+            vm.$swal({
+                toast: true,
+                position: "top-end",
+                icon: "error",
+                title: message,
+            });
+        }
     },
     rules :{
-      only_numbers : [
-        value =>
-          !!value || "Campo es requerido",
-        value => {
-          const pattern = /^\d+$/
-          return pattern.test(value) || 'Este campo solo admite numeros'
-        }
-      ],
-      required: [
-        value =>
-        !!value || "Campo es requerido",
-      ],
-      fail: [
-        value =>
-        false || "El campo es invalido",
-      ]
+        file_size_200kb: [
+            value => !value || value.size < 200000 || 'El archivo debe pesar menos de 200 kb!',
+        ],
+        file_size_400kb: [
+            value => !value || value.size < 400000 || 'El archivo debe pesar menos de 400 kb!',
+        ],
+        only_numbers : [
+            value => !!value || "Campo es requerido",
+            value => {
+                const pattern = /^\d+$/
+                return pattern.test(value) || 'Este campo solo admite numeros'
+            }
+        ],
+        required: [
+            value => !!value || "Campo es requerido",
+        ],
+        fail: [
+            value => false || "El campo es invalido",
+        ]
     },
     const:{
-      TIMEOUT:1000,
-      REFRESH:100,
+        TIMEOUT:1000,
+        REFRESH:100,
     },
 }
 export default UTILS;
