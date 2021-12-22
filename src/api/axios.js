1010b6305ca0b209a57f4a91d645d04ab5df7b35
unknown <fabriciovt@gmail.com>
2021-12-22 10:00:32 -0500

    auth

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 // const baseURL = 'http://lavikinga.bytesoluciones.com/';
-const baseURL = 'http://admin-lavikinga.bytesoluciones.test/';
+//const baseURL = 'http://admin-lavikinga.bytesoluciones.test/';
 
-// export const baseURL = 'http://admin_la_vikinga.test/';
+export const baseURL = 'http://admin_la_vikinga.test/';
 const headers = { 
     'Accept': 'aplicaition/json' ,
     'Content-Type': 'application/json'
