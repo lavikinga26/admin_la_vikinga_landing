2948a40680fcec3b3e5666f3923cb943dfa25e70
Alexander <acunaramirezalexander487@gmail.com>
2021-12-25 09:23:41 -0500

    Cambios en testimonios

--- src/pages/Testimonial/TestimonialPage.vue
+++ src/pages/Testimonial/TestimonialPage.vue
@@ -16,13 +16,13 @@
         <!-- Fin -->
         <div v-if="cant > 0" class="container-fluig pt-3 pb-5">
             <v-row class="sectionTestimonial" style="background-color: #0b152a">
-                <v-col cols="12" md="6" sm="12" class="d-flex align-center justify-center flex-column">
+                <v-col cols="12" md="6" sm="12" class="d-flex align-center justify-center flex-column dataSection">
                     <div class="text-center">
                         <p class="tit_h1_pink text_entrena pt-4">
                             {{ mainTestimonial.title }}
                         </p>
                     </div>
-                    <div class="text-center px-16">
+                    <div class="text-center px-5">
                         <p style="color: white">
                             {{ mainTestimonial.description }}
                         </p>
@@ -33,23 +33,20 @@
                     <iframe width="100%" height="100%" :src="loadURL(mainTestimonial.link).embed + '?autoplay=1&mute=0'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </v-col>
             </v-row>
-                <v-col cols="12" md="12" sm="12" class="gallery pt-5">
+            <v-row>
+                <v-col cols="12" md="12" sm="12" class="gallery pt-1">
                     <div v-for="testimonial in testimonialsData" :key="testimonial.id" class="testimonialItem">
                         <div class="testimonialVideo">
                             <iframe width="100%" height="100%" :src="loadURL(testimonial.link).embed" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
                         <div class="testimonialFooter">
                             <div>
-                                <p>
-                                    <strong>{{testimonial.title}}</strong>
-                                </p>         
-                            </div>
-                            <div>
                                 <v-btn
                                 fab
                                 dark
-                                small
+                                x-small
                                 color="pink"
+                                @click.stop="openDialog(testimonial)"
                                 >
                                     <v-icon dark>
                                         mdi-plus
@@ -59,14 +56,54 @@
                         </div>
 
                     </div>
-                </v-col>
-            <v-row>
-                
+                </v-col>          
             </v-row>
         </div>
         <div v-else class="container-fluig pt-3 pb-5">
             Comming soon
         </div>
+        <v-dialog
+        v-model="dialog"
+        hide-overlay
+        fullscreen
+        scrollable
+        >
+            <v-card style="background-color: #0b152a" >
+                <v-row>
+                    <v-col cols="12" md="9" sm="12" class="d-flex align-center videoMain">
+                        <iframe width="100%" height="100%" :src="currentTestimonial.link" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    
+                    </v-col>
+
+                    <v-col cols="12" md="3" sm="12" class="d-flex align-center justify-center flex-column">
+                        <div class="text-center">
+                            <p class="tit_h1_pink text_entrena pt-4">
+                                {{ currentTestimonial.title }}
+                            </p>
+                        </div>
+                        <div class="text-center px-5">
+                            <p style="color: white">
+                                {{ currentTestimonial.description }}
+                            </p>
+                        </div>
+
+                        <div class="d-flex justify-end">
+                            <v-btn
+                                fab
+                                dark
+                                x-small
+                                color="pink"
+                                @click.stop="dialog = !dialog; currentTestimonial = {}"
+                                >
+                                    <v-icon dark>
+                                        mdi-close
+                                    </v-icon>
+                                </v-btn>
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
@@ -77,7 +114,9 @@ export default {
             showTestimonials:false,
             cant: 0,
             testimonialsData:[],
-            mainTestimonial:{}
+            mainTestimonial:{},
+            dialog:false,
+            currentTestimonial:{},
         }
     },
     methods:{
@@ -106,6 +145,12 @@ export default {
             data.normal = "https://youtu.be/" + YId;
             return data;
         },
+        openDialog(t){
+            this.currentTestimonial = t;
+            this.currentTestimonial.link = this.loadURL(t.link).embed;
+            //console.log(this.currentTestimonial)
+            this.dialog = true;
+        }
     },
     mounted(){
         this.getTestimonials();
@@ -116,8 +161,8 @@ export default {
 <style>
 .gallery{
     display: grid;
-    gap: 1rem;
-    grid-auto-rows: 15rem;
+    gap: 0.1rem;
+    grid-auto-rows: 18rem;
     grid-template-columns: repeat(auto-fill,minmax(22rem, 1fr));
 }
 .alignCenter{
@@ -136,26 +181,30 @@ export default {
 }
 .testimonialVideo{
     width: 100%;
-    height: 80%;
+    height: 85%;
 }
 .testimonialFooter{
-    height: 20%;
+    height: 15%;
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-end;
     padding: 10px 15px;
     align-items: center;
+}
+.dataSection{
+    
 }
 .videoMain{
     height: auto;
 }
  @media (max-width: 960px) {
     .sectionTestimonial{
-        
         height: auto;
     }
     .videoMain{
         min-height: 350px;
-
+    }
+    .dataSection{
+        padding: 10px 100px;
     }
  } 
   @media (max-width: 650px) {
