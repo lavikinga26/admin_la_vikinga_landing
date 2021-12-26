8d04a6bf013fbbdf100060c34378390884dfa780
Erika M. Ramos <emachaca@peruapps.com.pe>
2021-12-26 00:56:21 -0500

    integracion subir comprobante de pago

--- src/pages/Shop/PaymentPage.vue
+++ src/pages/Shop/PaymentPage.vue
@@ -3,11 +3,17 @@
         <v-container>
             <div class="d-flex justify-center">
                 <v-card max-width="500" min-width="500" elevation="1" class="pa-5">
-                    <div class="text-center py-2">           
-                        <h3 style="font-weight: 400;">
-                            Información de la facturación
-                        </h3>
-                        <h5>N° Orden #00001002</h5>
+                    <div class="py-2 d-flex align-center">
+                        <div style="width: 40%" class="text-center">
+                            <img style="width: 100px" src="@/assets/img/icons/mobile-payment.png" />
+                        </div>   
+                        <div style="width: 60%" class="text-rigth">
+                            <h3 style="font-weight: 400;">
+                                Información de facturación
+                            </h3>
+                            <h5>N° Orden #00001002</h5>
+                        </div>     
+                        
                     </div>
                     <br>
                     <div>
@@ -16,26 +22,49 @@
                         <div>Dirección:<span style="float: right;">{{order.customer_address}}</span></div>
                     </div>
                     <br>
-                    <v-file-input
-                        :rules="rules"
-                        accept="image/png, image/jpeg, image/bmp"
-                        placeholder="Imágen comprobante"
-                        prepend-icon="mdi-camera"
-                        label="Imágen comprobante"
-                    ></v-file-input>
-
-                    <v-btn block dark color="secondary">
-                        <v-icon>mdi-tray-arrow-up</v-icon>
-                        &nbsp;&nbsp;ACTUALIZAR
-                    </v-btn>
+                    <template v-if="order.status == 4">
+                        <h3 class="text-center my-7"><b>Orden en pendiente de confirmación</b></h3>
+                    </template>
+                    <template v-if="order.status == 5">
+                        <v-file-input label="Imágen comprobante"
+                            accept="image/png, image/jpeg, image/bmp"
+                            ref="file"
+                            @change="onFileChange"
+                            prepend-icon="mdi-camera"
+                            placeholder="Imágen comprobante"
+                        ></v-file-input>
+                        <v-img :src="img_url"
+                            contain
+                            max-height="300"
+                            max-width="600"
+                            class="ma-5"
+                        />
+                        <v-btn block dark color="secondary" @click="submitFiles()">
+                            <v-icon>mdi-tray-arrow-up</v-icon>
+                            &nbsp;&nbsp;ACTUALIZAR
+                        </v-btn>
+                    </template>
                 </v-card>
             </div>
+            
+            <v-snackbar
+                v-model="toast.toast"
+                :timeout="toast.timeout"
+                :color="toast.color"
+                dark
+                >
+                {{ toast.message }}
+            </v-snackbar>
+
         </v-container>
     </div>
 </template>
 <script>
+import axios from "axios";
+import API from "../../api/axios";
 export default {
     components: { 
+        axios
      },
 
     data: () => ({
@@ -46,6 +75,18 @@ export default {
         rules: [
             value => !value || value.size < 2000000 || '',
         ],
+        
+        img_file: null,
+        img_url: "../images/default-image.png",
+        
+        toast:{
+            toast: false,
+            message: '',
+            timeout: 3000,
+            color: "success"
+        },
+
+        uploadSuccess: false,
     }),
 
     
@@ -59,6 +100,13 @@ export default {
     },
 
     methods: {
+        
+        showToast(msg,color){
+            this.toast.color = color;
+            this.toast.message = msg;
+            this.toast.toast = true;
+        },
+
         async getOrder(){
             let vm = this;
             vm.$store.commit('loader',true);
@@ -72,7 +120,34 @@ export default {
                 console.error(e);
                 vm.$store.commit('loader',false);
             }
-        }
+        },
+
+        onFileChange(file) {
+            if (!file) { return; }
+            this.img_file = this.$refs.file.lazyValue;
+            this.img_url = URL.createObjectURL(this.img_file);
+        },
+
+        async submitFiles() {
+            let formData = new FormData();
+            formData.append('id_order', this.slug);
+            formData.append('file', this.img_file);
+            axios.post('http://admin-lavikinga.bytesoluciones.test' + '/api/v1/files/upload-order-voucher',
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(function(data) {
+                //console.log(data.data);
+                this.uploadSuccess = true;
+                this.showToast('Comprobante enviado correctamente',"success");
+                this.$router.go();
+            }.bind(this)).catch(function(e) {
+                this.showToast(e.response.data.message,"red");
+            });
+        },
     },
 }
 </script>
