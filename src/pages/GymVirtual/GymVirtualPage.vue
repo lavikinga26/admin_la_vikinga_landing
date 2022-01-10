ec276100211ed286df3e40615f47b076990a66c0
Erika M. Ramos <erikamramos95@gmail.com>
2022-01-09 22:51:58 -0500

    Pagina de mantenimiento

--- src/pages/GymVirtual/GymVirtualPage.vue
+++ src/pages/GymVirtual/GymVirtualPage.vue
@@ -25,20 +25,30 @@
                 <!--<v-icon>mdi-view-agenda-outline</v-icon>-->
                 MI AGENDA
             </v-btn>
-            <span style="color: #fff; font-size: 0.9rem;"> | </span>
+            <!--<span style="color: #fff; font-size: 0.9rem;"> | </span>
             <v-btn 
                 text 
                 small
                 link
                 :to="'/gym-virtual/galeria'"
             >
-                <!--<v-icon>mdi-animation-play-outline</v-icon>-->
                 GALERÍA
-            </v-btn>
+            </v-btn>-->
         </v-toolbar>
 
-        <div class="d-flex flex-grow-1 flex-column">
+        <!--<div class="d-flex flex-grow-1 flex-column">
             <router-view :key="$route.fullPath" class="flex-grow-1"></router-view>
+        </div>-->
+        <div class="d-flex flex-grow-1 flex-column">
+            <v-container style="min-height:70vh;" class="d-flex align-center">
+                <div style="text-align: center; width: 100%" class="pa-10">
+                    <img style="width: 250px;" src="@/assets/img/logo_vikinga_icon.png" alt="Logo" />
+                    <p class="tit_h1_staff_pink text_entrena txt_uppercase mb-6" style="font-size: 5rem">
+                        PRÓXIMAMENTE
+                    </p> 
+                </div>
+                
+            </v-container>
         </div>
     </div>
 </template>
