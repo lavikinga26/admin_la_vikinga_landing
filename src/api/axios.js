5f1ad763ce4ff21d2f62a39c8a6c5bd48f1e74fb
Erika M. Ramos <erikamramos95@gmail.com>
2024-11-06 10:34:42 -0500

    local

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,9 @@ import axios from "axios";
 //export const baseURL = 'http://admin_la_vikinga.test/';
 // export const baseURL = 'http://adminlavikinga.test/';
 // export const baseURL = 'https://apiserv.lavikingaoficial.com/';
-export const baseURL = 'https://apiweb.lavikingaoficial.com/';
+// export const baseURL = 'https://apiweb.lavikingaoficial.com/';
+export const baseURL = 'http://127.0.0.1:8000';
+
 const headers = { 
     'Accept': 'application/json',//'*' ,
     'Content-Type': 'application/json',
